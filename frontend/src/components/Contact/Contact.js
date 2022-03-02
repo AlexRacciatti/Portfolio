86d33bd9c2383ec43017a1d9cba39ff0e97cb1e7
Alex Racciatti <racciattialex@gmail.com>
2022-03-02 18:30:18 -0300

    Feat: Add email JS
    Now users can email me directly by filling a form in Contact component. STYLES pending

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Header from '../Home/Header/Header';
 
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ewu43hu', 'template_u9mo2ah', form.current, 'ZFViWuqS2r_ortz1s')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return(
     <div>
         <Header/>
         <h1>This is Contact Component</h1>
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
     </div>
   )
 };
