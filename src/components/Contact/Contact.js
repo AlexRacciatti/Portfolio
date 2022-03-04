79bae4321b199631286d54add4f4b3f26b2d2414
Alex Racciatti <racciattialex@gmail.com>
2022-03-04 18:56:37 -0300

    Feat: Added logos of LinkedIn and GitHub
    Now users can click on this logos to go to my profile of each platform

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,60 +4,57 @@ import './Contact.css'
 
 import emailjs from '@emailjs/browser';
 import Swal from 'sweetalert2';
+import { GitHub, LinkedIn } from '@mui/icons-material';
 
 const Contact = () => {
-
+  
   const form = useRef();
-
+  
   const sendEmail = (e) => {
     e.preventDefault();
-
+    
     emailjs.sendForm('service_ewu43hu', 'template_u9mo2ah', form.current, 'ZFViWuqS2r_ortz1s')
-      .then((result) => {
-          form.current.reset()
-          console.log(result.text);
-          Swal.fire(
-            'Thank you!',
-            'Your message was succesfully sent. You will recieve an answer ASAP',
-            'success'
-          )
+    .then((result) => {
+      form.current.reset()
+      console.log(result.text);
+      Swal.fire(
+        'Thank you!',
+        'Your message was succesfully sent. You will recieve an answer ASAP',
+        'success'
+        )
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
-  };
-
-  return(
-    <div>
+    };
+    
+    return(
+      <div>
         <Header/>
-        <h1>This is Contact Component</h1>
-        <div className='contact-container'>
-          <div className='left'>
-            <div className='contact-form-background'>
-              <h2>Let me know if you want me to reach you!</h2>
-              <h4>Complete this form so i can get in touch</h4>
-              <form ref={form} onSubmit={sendEmail} className='contact-form'>
-                <label>Name</label>
-                <input type="text" name="user_name" className='contact-input' required />
-                <label>Email</label>
-                <input type="email" name="user_email" className='contact-input' required />
-                <label>Message</label>
-                <textarea name="message" className='contact-input' required />
-                <button type='submit'>Submit</button>
-              </form>
-            </div>
-          </div>
-          <div className='right'>
-            <h2>If you want to see more about me, here are my social medias</h2>
-            <ul>
-              <li>LinkedIn</li>
-              <li>GitHub</li>
-              <li>Twitter</li>
-            </ul>
-          </div>
+        <div className='contact-form-content'>
+          <h1 className='contact-heading'>Contact me!</h1>
+          <h2 className='contact-heading contact-form-subtitle'>Complete this form to send me a message to my email 😄</h2>
+          <form ref={form} onSubmit={sendEmail} className='contact-form'>
+            <label>Name</label>
+            <input type="text" name="user_name" className='contact-input' required />
+            
+            <label>Email</label>
+            <input type="email" name="user_email" className='contact-input' required />
+            
+            <label>Message</label>
+            <textarea name="message" className='contact-input' required />
+            
+            <button className='contact-submit-btn' type='submit'>Submit</button>
+          </form>
+          <h2 className='contact-heading contact-form-subtitle'>Feel free to connect with me at LinkedIn and view my code at GitHub</h2>
+          <ul className='contact-socials-list'>
+            <li ><a className='contact-social-item' href='https://www.linkedin.com/in/alexracciatti/' target={'_blank'} rel='noreferrer'><LinkedIn  style={{ fontSize: '70px' }}/></a></li>
+            <li ><a className='contact-social-item' href='https://github.com/AlexRacciatti' target={'_blank'} rel='noreferrer'><GitHub  style={{ fontSize: '70px' }}/></a></li>
+          </ul>
         </div>
-        
-    </div>
-  )
-};
-
-export default Contact;
+      
+      </div>
+      )
+    };
+    
+    export default Contact;
+    
